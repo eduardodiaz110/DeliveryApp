13064a76070ad620e26c2aa09cc1706ef034a1ed
Víctor Ponce <107383836+victor-ponce@users.noreply.github.com>
2023-10-30 11:51:54 -0600

    Native Base Installation

--- App.tsx
+++ App.tsx
@@ -1,24 +1,23 @@
-import * as React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { Navigator, RootStackParams } from "./Navigator";
-import { Text } from "react-native";
-import HomeScreen from "./screens/HomeScreen";
-import Prueba from "./screens/Prueba";
-import { AuthProvider } from "./auth/AuthContext";
-import { AppProvider } from "./AppContext";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { createStackNavigator } from "@react-navigation/stack";
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { Navigator, RootStackParams } from './Navigator';
+import { AuthProvider } from './auth/AuthContext';
+import { AppProvider } from './AppContext';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NativeBaseProvider } from 'native-base';
 
 const RootTab = createStackNavigator<RootStackParams>();
 
 export default function App() {
   return (
-    <AuthProvider>
-      <AppProvider>
-        <NavigationContainer>
-          <Navigator />
-        </NavigationContainer>
-      </AppProvider>
-    </AuthProvider>
+    <NativeBaseProvider>
+      <AuthProvider>
+        <AppProvider>
+          <NavigationContainer>
+            <Navigator />
+          </NavigationContainer>
+        </AppProvider>
+      </AuthProvider>
+    </NativeBaseProvider>
   );
 }
